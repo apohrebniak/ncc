4ac7a3203d1c9ec985bf5d8da05ba3897320eac6
apohrebniak <andrii.pohrebniak@gmail.com>
2017-10-16 23:37:19 +0300

    fix

--- ncc/Lexer.py
+++ ncc/Lexer.py
@@ -2,9 +2,8 @@ import sys
 
 
 class Lexer:
-
-    LSB, RSB, LFB, RFB, LB, RB, WHILE, DO, IN, OUT, OR, AND, NOT,\
-    INT, FLOAT, IF, DOTS, EQ, PLUS, MINUS, MUL, DIV, NEQ, LOW, LOWEQ,\
+    LSB, RSB, LFB, RFB, LB, RB, WHILE, DO, IN, OUT, OR, AND, NOT, \
+    INT, FLOAT, IF, DOTS, EQ, PLUS, MINUS, MUL, DIV, NEQ, LOW, LOWEQ, \
     HI, HIEQ, ID, CONST, NL, QM = range(31)
 
     SYMBOLS = {'[': LSB, ']': RSB, '{': LFB, '}': RFB, '(': LB, ')': RB,
@@ -35,97 +34,85 @@ class Lexer:
         else:
             self.char = self.char + 1
 
-
         return c
 
-    def state_1(self):
+    def scan(self):
         print("state_1")
-        sym = self.nextChar()
-        print('[',sym,']')
+        while True:
+            sym = self.nextChar()
+            print('[', sym, ']')
 
-        if sym.isalpha():
-            self.state_2()
-        elif sym.isdigit():
-            self.state_3()
-        elif sym == '.':
-            self.state_4()
-        elif sym == '!':
-            self.state_5()
-        elif sym == '=':
-            self.state_6()
-        elif sym == '<':
-            self.state_7()
-        elif sym == '>':
-            self.state_8()
-        elif sym in [',', '\n', '+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '?', ':']:
-            self.state_9()
-        else:
-            self.error()
+            if len(sym) == 0:
+                return
+
+            if sym.isalpha():
+                self.state_2()
+            elif sym.isdigit():
+                self.state_3()
+            elif sym == '.':
+                self.state_4()
+            elif sym == '!':
+                self.state_5()
+            elif sym == '=':
+                self.state_6()
+            elif sym == '<':
+                self.state_7()
+            elif sym == '>':
+                self.state_8()
+            elif sym in [',', '\n', '+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '?', ':']:
+                self.state_9()
+            else:
+                self.error()
 
         pass
 
     def state_2(self):
         print("state2")
         sym = self.nextChar()
-        print('[',sym,']')
+        print('[', sym, ']')
 
         if sym.isalpha() or sym.isdigit():
             self.state_2()
-        self.state_1()
 
     def state_3(self):
         print("state3")
         sym = self.nextChar()
-        print('[',sym,']')
+        print('[', sym, ']')
 
         if sym.isdigit():
             self.state_3()
         elif sym == '.':
             self.state_4()
 
-        self.state_1()
-
     def state_4(self):
         print("stage4")
         sym = self.nextChar()
-        print('[',sym,']')
+        print('[', sym, ']')
 
         if sym.isdigit():
             self.state_3()
         else:
             self.error()
 
-        self.state_1()
-
     def state_5(self):
         print("stage5")
         sym = self.nextChar()
-        print('[',sym,']')
-
-        self.state_1()
+        print('[', sym, ']')
 
     def state_6(self):
         print("stage6")
         sym = self.nextChar()
-        print('[',sym,']')
-
-        self.state_1()
+        print('[', sym, ']')
 
     def state_7(self):
         print("stage7")
         sym = self.nextChar()
-        print('[',sym,']')
-
-        self.state_1()
+        print('[', sym, ']')
 
     def state_8(self):
         print("stage8")
         sym = self.nextChar()
-        print('[',sym,']')
-
-        self.state_1()
+        print('[', sym, ']')
 
     def state_9(self):
         print("state9")
-
-        self.state_1()
--- ncc/main.py
+++ ncc/main.py
@@ -7,7 +7,7 @@ import ncc.Lexer as Lexer
 def main():
     stream = open(sys.argv[1])
     lexer = Lexer.Lexer(stream)
-    lexer.state_1()
+    lexer.scan()
 
 
 if __name__ == "__main__":
